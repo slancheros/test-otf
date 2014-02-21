6d37fdd27566788fc63329f254bb7e841c2537c5
slancheros <sandrallancherosg@hotmail.com>
2014-02-20 23:22:07 -0500

    InitialVersionOTF

---
+++ OTF/Controllers/OTFController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OTF.Controllers
+{
+    public class OTFController : Controller
+    {
+        //
+        // GET: /OTF/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Find()
+        {
+            return View();
+        }
+
+        public ActionResult Offer()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OTF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OTF.MvcApplication" Language="C#" %>
